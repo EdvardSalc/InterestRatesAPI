70f6ef28352cf189d9cfb30566dc25708ff3cdc7
Edvardas <edvard.salc@gmail.com>
2020-10-10 16:15:57 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Data.Context;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace InterestRatesAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        AgreementHandler agreementHandler = new AgreementHandler();
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Demo Page";
+
+            return View();
+        }
+
+        public ActionResult GetAgreementData()
+        {
+            var query = agreementHandler.GetAgreements();
+            var agreementsIds = agreementHandler.GetAgreements().Select(a => new { Id = a.Id });
+
+            return Json(agreementsIds.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Controllers/InterestRateController.cs
@@ -0,0 +1,58 @@
+﻿using Data.Context;
+using Data.DTO;
+using Data.Models;
+using InterestRatesAPI.Services;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InterestRatesAPI.Controllers
+{
+    public class InterestRateController : ApiController
+    {
+        private AgreementHandler agreementHandler = new AgreementHandler();
+        private CustomerHandler customerHandler= new CustomerHandler();
+        private InterestRateService interestRateService = new InterestRateService();
+
+            public HttpResponseMessage GetInterestRate(int agreementId, string baseRateCode)
+        {
+
+           
+
+            RateResponse response = new RateResponse();
+            try
+            {
+                AgreementDTO agreement = agreementHandler.GetAgreement((int)agreementId);
+
+               bool isValid = interestRateService.ValidateRequestParams(agreement, baseRateCode);
+
+                if (isValid)
+                {
+                    CustomerDTO customer = customerHandler.GetCustomerByAgreementId(agreementId);
+                    response = interestRateService.GetRatesInformation(agreement, baseRateCode);
+                    response.agreement = agreement;
+                    response.customer = customer;
+
+                    return Request.CreateResponse(HttpStatusCode.OK, response, Configuration.Formatters.JsonFormatter);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Wrong parameters", Configuration.Formatters.JsonFormatter);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, e.InnerException, Configuration.Formatters.JsonFormatter);
+
+            }
+
+            
+        }
+
+
+
+    }
+}
